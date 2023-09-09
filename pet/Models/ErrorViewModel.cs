40e46f94ec01493ba71e9df7a2795865d9c8717b
Tiara <Tiara@TIARADASHOL>
2023-09-09 18:38:37 +0700

    Add project files.

---
+++ pet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace pet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
